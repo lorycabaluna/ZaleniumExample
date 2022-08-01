08e85261a98b520fe4367ccd4cb0195ab3d9ea16
Lory <johntxt_2006@yahoo.com>
2022-08-01 10:59:03 +0800

    test1

--- src/test/java/ZaleniumDemo/ON_OFF_Zalenium.java
+++ src/test/java/ZaleniumDemo/ON_OFF_Zalenium.java
@@ -8,24 +8,24 @@ import org.testng.annotations.BeforeTest;
 public class ON_OFF_Zalenium {
 	
 	
-	@BeforeTest
-	public void start_Zalenium() throws IOException, InterruptedException {
-		
-		//Runtime.getRuntime().exec("open /Users/macbookpro152014retina/Desktop/Zalenium_start.command");
-		Runtime.getRuntime().exec("open /Users/macbookpro152014retina/eclipse-workspace/ZaleniumTest/ZaleniumStart_Stop/Zalenium_start.command");
-		//Runtime.getRuntime().exec("open ZaleniumStart_Stop/Zalenium_start.command");
-		
-		Thread.sleep(15000);
-	}
-	
-	@AfterTest
-	public void stop_Zalenium() throws IOException, InterruptedException {
-		
-		//Runtime.getRuntime().exec("open /Users/macbookpro152014retina/Desktop/Zalenium_stop.command");
-		Runtime.getRuntime().exec("open /Users/macbookpro152014retina/eclipse-workspace/ZaleniumTest/ZaleniumStart_Stop/Zalenium_stop.command");
-		//Runtime.getRuntime().exec("open /ZaleniumStart_Stop/Zalenium_stop.command");
-		
-		Thread.sleep(10000);
-	}
+//	@BeforeTest
+//	public void start_Zalenium() throws IOException, InterruptedException {
+//		
+//		//Runtime.getRuntime().exec("open /Users/macbookpro152014retina/Desktop/Zalenium_start.command");
+//		Runtime.getRuntime().exec("open /Users/macbookpro152014retina/eclipse-workspace/ZaleniumTest/ZaleniumStart_Stop/Zalenium_start.command");
+//		//Runtime.getRuntime().exec("open ZaleniumStart_Stop/Zalenium_start.command");
+//		
+//		Thread.sleep(15000);
+//	}
+//	
+//	@AfterTest
+//	public void stop_Zalenium() throws IOException, InterruptedException {
+//		
+//		//Runtime.getRuntime().exec("open /Users/macbookpro152014retina/Desktop/Zalenium_stop.command");
+//		Runtime.getRuntime().exec("open /Users/macbookpro152014retina/eclipse-workspace/ZaleniumTest/ZaleniumStart_Stop/Zalenium_stop.command");
+//		//Runtime.getRuntime().exec("open /ZaleniumStart_Stop/Zalenium_stop.command");
+//		
+//		Thread.sleep(10000);
+//	}
 
 }
